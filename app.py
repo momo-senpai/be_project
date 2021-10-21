fd8a51e8442685f7d485595942b7d114edf5ef9d
A Aditya <52173972+momo-senpai@users.noreply.github.com>
2021-10-21 12:46:16 +0530

    making changes to pattern finder

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import pandas as pd
 # from statsmodels.nonparametric.kernel_regression import KernelReg
 # from fbprophet import Prophet
 
+
 app = Flask(__name__)
 
 
@@ -221,163 +222,11 @@ def prediction():
 
         except Exception as e:
             print("Failed to get required data.", e)
-def pred():
 
-    def plot_window(prices, extrema, smooth_prices, smooth_extrema, ax=None):
-    
-        if ax is None:
-            fig = plt.figure()
-            ax = fig.add_subplot(111)
-
-        prices.plot(ax=ax, color='dodgerblue')
-        ax.scatter(extrema.index, extrema.values, color='red')
-        smooth_prices.plot(ax=ax, color='lightgrey')
-        ax.scatter(smooth_extrema.index, smooth_extrema.values, color='lightgrey')
-    
-    def find_patterns(extrema, max_bars=35):
-        patterns = defaultdict(list)
-
-    # Need to start at five extrema for pattern generation
-        for i in range(5, len(extrema)):
-            window = extrema.iloc[i-5:i]
-
-        # A pattern must play out within max_bars (default 35)
-            if (window.index[-1] - window.index[0]) > max_bars:
-                continue
-
-        # Using the notation from the paper to avoid mistakes
-        e1 = window.iloc[0]
-        e2 = window.iloc[1]
-        e3 = window.iloc[2]
-        e4 = window.iloc[3]
-        e5 = window.iloc[4]
-
-        rtop_g1 = np.mean([e1, e3, e5])
-        rtop_g2 = np.mean([e2, e4])
-        # Head and Shoulders
-        if (e1 > e2) and (e3 > e1) and (e3 > e5) and \
-                (abs(e1 - e5) <= 0.03*np.mean([e1, e5])) and \
-                (abs(e2 - e4) <= 0.03*np.mean([e1, e5])):
-            patterns['HS'].append((window.index[0], window.index[-1]))
-
-        # Inverse Head and Shoulders
-        elif (e1 < e2) and (e3 < e1) and (e3 < e5) and \
-                (abs(e1 - e5) <= 0.03*np.mean([e1, e5])) and \
-                (abs(e2 - e4) <= 0.03*np.mean([e1, e5])):
-            patterns['IHS'].append((window.index[0], window.index[-1]))
-
-        # Broadening Top
-        elif (e1 > e2) and (e1 < e3) and (e3 < e5) and (e2 > e4):
-            patterns['BTOP'].append((window.index[0], window.index[-1]))
-
-        # Broadening Bottom
-        elif (e1 < e2) and (e1 > e3) and (e3 > e5) and (e2 < e4):
-            patterns['BBOT'].append((window.index[0], window.index[-1]))
-
-        # Triangle Top
-        elif (e1 > e2) and (e1 > e3) and (e3 > e5) and (e2 < e4):
-            patterns['TTOP'].append((window.index[0], window.index[-1]))
-
-        # Triangle Bottom
-        elif (e1 < e2) and (e1 < e3) and (e3 < e5) and (e2 > e4):
-            patterns['TBOT'].append((window.index[0], window.index[-1]))
-
-        # Rectangle Top
-        elif (e1 > e2) and \
-                (abs(e1-rtop_g1)/rtop_g1 < 0.0075) and \
-                (abs(e3-rtop_g1)/rtop_g1 < 0.0075) and \
-                (abs(e5-rtop_g1)/rtop_g1 < 0.0075) and \
-                (abs(e2-rtop_g2)/rtop_g2 < 0.0075) and \
-                (abs(e4-rtop_g2)/rtop_g2 < 0.0075) and \
-                (min(e1, e3, e5) > max(e2, e4)):
-
-            patterns['RTOP'].append((window.index[0], window.index[-1]))
-
-        # Rectangle Bottom
-        elif (e1 < e2) and \
-                (abs(e1-rtop_g1)/rtop_g1 < 0.0075) and \
-                (abs(e3-rtop_g1)/rtop_g1 < 0.0075) and \
-                (abs(e5-rtop_g1)/rtop_g1 < 0.0075) and \
-                (abs(e2-rtop_g2)/rtop_g2 < 0.0075) and \
-                (abs(e4-rtop_g2)/rtop_g2 < 0.0075) and \
-                (max(e1, e3, e5) > min(e2, e4)):
-
-            patterns['RBOT'].append((window.index[0], window.index[-1]))
-
-        return patterns
-
-    def find_extrema(s, bw='cv_ls'):
+@app.route('/finder', methods=['GET', "POST"])
+def finder():
     
     
-        prices = s.copy()
-        prices = prices.reset_index()
-        prices.columns = ['date', 'price']
-        prices = prices['price']
-
-        kr = KernelReg(
-            [prices.values],
-            [prices.index.to_numpy()],
-            var_type='c', bw=bw
-        )
-        f = kr.fit([prices.index])
-
-        # Use smoothed prices to determine local minima and maxima
-        smooth_prices = pd.Series(data=f[0], index=prices.index)
-        smooth_local_max = argrelextrema(smooth_prices.values, np.greater)[0]
-        smooth_local_min = argrelextrema(smooth_prices.values, np.less)[0]
-        local_max_min = np.sort(
-            np.concatenate([smooth_local_max, smooth_local_min]))
-        smooth_extrema = smooth_prices.loc[local_max_min]
-
-        # Iterate over extrema arrays returning datetime of passed
-        # prices array. Uses idxmax and idxmin to window for local extrema.
-        price_local_max_dt = []
-        for i in smooth_local_max:
-            if (i > 1) and (i < len(prices)-1):
-                price_local_max_dt.append(prices.iloc[i-2:i+2].idxmax())
-
-        price_local_min_dt = []
-        for i in smooth_local_min:
-            if (i > 1) and (i < len(prices)-1):
-                price_local_min_dt.append(prices.iloc[i-2:i+2].idxmin())
-
-        maxima = pd.Series(prices.loc[price_local_max_dt])
-        minima = pd.Series(prices.loc[price_local_min_dt])
-        extrema = pd.concat([maxima, minima]).sort_index()
-
-        return extrema, prices, smooth_extrema, smooth_prices
-    
-    googl = yf.download('AAPL', start='2020-01-01', end='2020-01-31')
-    googl.drop(['Adj Close'], axis=1, inplace=True)
-    prices, extrema, smooth_prices, smooth_extrema = find_extrema(googl['Close'], bw=[1.5])
-    patterns = find_patterns(extrema)
-
-    for name, pattern_periods in patterns.items():
-        print(f"{name}: {len(pattern_periods)} occurences")
-    
-    print(patterns.items)
-    for name, pattern_periods in patterns.items():
-        if name=='TBOT':
-            print(name)
-
-            rows = int(np.ceil(len(pattern_periods)/2))
-            print(rows)
-            f, axes = plt.subplots(rows, 2, figsize=(20,5*rows))
-            axes = axes.flatten()
-            i = 0
-            for start, end in pattern_periods:
-                s = prices.index[0]
-                e = prices.index[len(prices.index)-1]
-                plot_window(prices[s:e], extrema.loc[s:e],
-                                smooth_prices[s:e],
-                                smooth_extrema.loc[s:e], ax=axes[i])
-                i+=1
-                plt.show()
-
-    return render_template('prediction.html')
-
-
-
 
 @app.route('/about')
 def about():
